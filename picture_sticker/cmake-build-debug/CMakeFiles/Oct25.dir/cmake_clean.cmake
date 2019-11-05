aebf8a91f2762f2fe5ac3771bf16a888404eb3e0
miaotsi <32031885+miaotsi@users.noreply.github.com>
2019-11-05 20:14:38 +0800

    Add files via upload
    change main.cpp to picture_sticker.cpp in CMakeLists

---
+++ picture_sticker/cmake-build-debug/CMakeFiles/Oct25.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Oct25.dir/main.cpp.o"
+  "Oct25"
+  "Oct25.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Oct25.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
